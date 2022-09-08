7ec61fe72e04368a949f71a1c2e6be9a25f22ccc
Surya T N <suryatej.nim@yahoo.com>
2022-09-08 08:13:59 +0000

    Implemented logic to render and manage state within Capitals component, composed of a child component: CapitalItem

--- src/components/Capitals/index.js
+++ src/components/Capitals/index.js
@@ -1,3 +1,6 @@
+import {Component} from 'react'
+import './index.css'
+
 const countryAndCapitalsList = [
   {
     id: 'NEW_DELHI',
@@ -26,4 +29,61 @@ const countryAndCapitalsList = [
   },
 ]
 
-// Write your code here
+const getCapitalsCountryName = capitalNameId => {
+  const selectedItemCapitalCountryData = countryAndCapitalsList.find(
+    countryAndCapitalListItem => countryAndCapitalListItem.id === capitalNameId,
+  )
+
+  return selectedItemCapitalCountryData.country
+}
+
+const CapitalItem = props => {
+  const {itemData, isSelected} = props
+  const {id, capitalDisplayText} = itemData
+
+  const optionElementString = `<option className="capital-selection-dropdown-option" value=${id} ${
+    isSelected ? 'selected' : ''
+  }>${capitalDisplayText}</option>`
+  return {optionElementString}
+}
+
+export default class Capitals extends Component {
+  state = {
+    selectedCapitalId: countryAndCapitalsList[0].id,
+  }
+
+  onCapitalNameSelection = capitalNameSelectionEvent => {
+    const idOfCapitalNameSelectedByUser = capitalNameSelectionEvent.target.value
+
+    this.setState({
+      selectedCapitalId: idOfCapitalNameSelectedByUser,
+    })
+  }
+
+  render() {
+    const {selectedCapitalId} = this.state
+    const capitalsCountryName = getCapitalsCountryName(selectedCapitalId)
+
+    return (
+      <div className="capitals-app-bg-container">
+        <div className="content-container">
+          <h1 className="content-header">Countries And Capitals</h1>
+          <div className="capital-selection-container">
+            <select className="capital-selection-dropdown" name="capital-name">
+              {countryAndCapitalsList.map(capitalCountryDataItem => (
+                <CapitalItem
+                  itemData={capitalCountryDataItem}
+                  isSelected={capitalCountryDataItem.id === selectedCapitalId}
+                />
+              ))}
+            </select>
+            <p className="capital-selection-question">
+              is capital of which country?
+            </p>
+          </div>
+          <p className="country-text">{capitalsCountryName}</p>
+        </div>
+      </div>
+    )
+  }
+}
